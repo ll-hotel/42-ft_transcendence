54f3e552091e3c5bb6bf49a88dcd58b72fc30ae1
ll-hotel <ll-hotel@student.42lyon.fr>
2025-10-09 14:42:45 +0200

    feat!: authentication module routes with frontend api calls

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,30 +1,25 @@
-import Fastify, { FastifyInstance } from 'fastify'
-import fs from 'fs'
-import { auth } from './auth';
-import { STATUS } from './shared';
+import Fastify, { FastifyInstance } from "fastify"
+import { auth } from "./auth";
+import { STATUS } from "./shared";
+import fs from "fs";
 
-const app: FastifyInstance = Fastify({});
-
-app.get('/', (req, res) => {
-    if (req.url === '/') {
-        const file: string = fs.readFileSync('www/index.html', 'utf-8');
-        res.header('content-type', 'text/html');
-        return file;
-    }
+const app: FastifyInstance = Fastify({
+	logger: true,
+	https: {
+		key: fs.readFileSync("/run/secrets/privatekey.pem"),
+		cert: fs.readFileSync("/run/secrets/certificate.pem"),
+	}
 });
 
-app.get('/ping', (_req, res) => {
-    res.code(STATUS.success).send('pong');
+app.get("/ping", (_req, res) => {
+    res.code(STATUS.success).send("pong");
 });
 
-async function main() {
-    try {
-        auth.setup(app);
-        await app.listen({ port: 8080, host: '0.0.0.0' });
-    } catch (err: any) {
-        console.log('Unhandled error caught in main:', err);
-        process.exit(1);
-    }
-}
+auth.setup(app);
 
-main();
+app.listen({ port: 8080, host: "0.0.0.0" }, function (err, _address) {
+	if (err) {
+		console.log("Could not start server:", err);
+		process.exit(1);
+	}
+});
--- frontend/src/request.ts
+++ frontend/src/request.ts
@@ -1,31 +1,29 @@
-export async function get(path: string, cache: RequestCache = "default"): Promise<JSON> {
-	const response = await fetch(`http://${window.location.hostname}/api/${encodeURI(path)}`, {
-		method: "GET",
-		headers: {
-			"Accept": "application/json",
-			"Content-Type": "application/json"
-		},
-		credentials: "include",
-		cache,
-	});
-	if (response.ok === false) {
-		return Promise.reject(response);
-	}
-	return response.json();
+type ApiResponse = { status: number, message: string };
+
+async function get(path: string, cache: RequestCache = "default"): Promise<ApiResponse> {
+    return await fetch(`https://${window.location.hostname}/${encodeURI(path)}`, {
+        method: "GET",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        credentials: "include",
+        cache,
+    }).then(function (res) {
+        return res.json() as any as ApiResponse;
+    });
 }
 
-export async function post(path: string, body: object = {}): Promise<JSON> {
-	const response = await fetch(`http://${window.location.hostname}/api/${encodeURI(path)}`, {
-		method: "POST",
-		headers: {
-			"Accept": "application/json",
-			"Content-Type": "application/json"
-		},
-		credentials: "include",
-		body: JSON.stringify(body)
-	});
-	if (response.ok === false) {
-		return Promise.reject(response);
-	}
-	return response.json();
+async function post(path: string, body: object = {}): Promise<ApiResponse> {
+    return await fetch(`https://${window.location.hostname}${encodeURI(path)}`, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        credentials: "include",
+        body: JSON.stringify(body)
+    }).then(function (res) {
+        return res.json() as any as ApiResponse;
+    });
 }
