a817eaa4b2f23ae4287c761f42f066032f5aec54
luc20131 <lucasrichaud02@gmail.com>
2025-11-16 06:03:22 +0100

    j'ai cree les paddles (fixe) et le system de scoring (un peu basique)

--- frontend/src/pong.ts
+++ frontend/src/pong.ts
@@ -1,17 +1,17 @@
 type Position = {x: number, y: number};
 type Size = {w: number, h: number};
+type Score = {p1: number, p2: number};
 
-
-	function dot_product(x1: number, y1: number, x2: number, y2: number) : number
-	{
-		return ((x1 * x2) + (y1 * y2));
-	}
+function dot_product(x1: number, y1: number, x2: number, y2: number) : number
+{
+	return ((x1 * x2) + (y1 * y2));
+}
 
 abstract class PhysicObject
 {
-	protected position: Position;
-	protected speed: Vector2D;
-	protected size: Size | number;
+	protected	position: Position;
+	protected	speed: Vector2D;
+	readonly	size: Size | number;
 
 	constructor(position: Position, size: Size | number, speed: Vector2D)
 	{
@@ -98,30 +98,39 @@ export class Game
 	private buffer: number;
 	private tick_interval: number;
 	private tick_rate: number;
+	private score: Score;
 
 	constructor(html: HTMLElement)
 	{
 		this.canvas = new PongCanvas(html.querySelector("#pong-canvas")!);
-		this.ball = new PongBall(this.canvas);
-		this.paddle_p1 = new PongPaddle({x: 10, y: 50});
-		this.paddle_p2 = new PongPaddle({x: 100, y: 50});
+		this.score= {p1: 0, p2: 0};
+		this.ball = new PongBall(this.canvas, this.score);
+		this.paddle_p1 = new PongPaddle({x: 0, y: this.canvas.canvas.height / 2});
+		this.paddle_p2 = new PongPaddle({x: this.canvas.canvas.width, y: this.canvas.canvas.height / 2});
 		this.is_running = false;
 		this.last_timestamp = performance.now();
 		this.buffer = 0;
-		this.tick_rate = 30;
+		this.tick_rate = 60;
 		this.tick_interval = 1000 / this.tick_rate;
 	}
 
 	summon_ball() : void
 	{
-		this.canvas.getContext().drawImage(this.ball.getTexture(), 10, 10);
-		this.ball.addSpeed(new Vector2D(3,3));
+		this.canvas.getContext().drawImage(this.ball.getTexture(), this.canvas.canvas.width / 2, this.canvas.canvas.height / 2, 20, 20);
+	}
+
+	summon_paddle() : void
+	{
+		this.canvas.getContext().drawImage(this.paddle_p1.getTexture(), this.paddle_p1.pos.x, (this.paddle_p1.pos.y - (this.paddle_p2.size as Size).h / 2), 20, 50);
+		this.canvas.getContext().drawImage(this.paddle_p2.getTexture(), this.paddle_p2.pos.x - (this.paddle_p2.size as Size).w, this.paddle_p2.pos.y - ((this.paddle_p2.size as Size).h / 2), 20, 50);
+
 	}
 
 	start() : void
 	{
 		console.log("BAAAAAAALLL");
 		this.summon_ball();
+		this.summon_paddle();
 		this.is_running = true;
 		this.loop(this.last_timestamp);
 	}
@@ -144,8 +153,9 @@ export class Game
 	update(t: number) : void
 	{
 		this.canvas.getContext().reset();
-		console.log("Tick ", t, "ms");
+	//	console.log("Tick ", t, "ms");
 		this.ball.updatePos();
+		this.summon_paddle();
 		this.canvas.getContext().drawImage(this.ball.getTexture(), this.ball.pos.x - 10, this.ball.pos.y - 10);
 	}
 }
@@ -156,14 +166,17 @@ export class PongCanvas
 	private context : CanvasRenderingContext2D;
 	readonly top_normal : Vector2D;
 	readonly bottom_normal : Vector2D;
+	readonly left_normal : Vector2D;
+	readonly right_normal : Vector2D;
 
 	constructor(canvas: HTMLCanvasElement)
 	{
 		this.canvas = canvas;
 		this.context = this.canvas.getContext("2d")!;
 		this.top_normal = new Vector2D(0, 1);
-		console.log("height",this.canvas.height);
 		this.bottom_normal = new Vector2D(0, -1);
+		this.left_normal = new Vector2D(1, 0);
+		this.right_normal = new Vector2D(-1, 0);
 	}
 
 	getContext() : CanvasRenderingContext2D
@@ -178,23 +191,31 @@ export class PongPaddle extends PhysicObject
 
 	constructor(position: Position)
 	{
-		super(position, {w: 30, h: 30},new Vector2D(0,0));
+		super(position, {w: 20, h: 50},new Vector2D(0,0));
 		this.texture = new Image();
 		this.texture.src = "/pong_bar.png";
 	}
+
+	getTexture() : HTMLImageElement
+	{
+		return (this.texture);
+	}
+
 }
 
 export class PongBall extends PhysicObject
 {
 	private texture: HTMLImageElement;
 	private canvas: PongCanvas;
+	private score: Score;
 
-	constructor(canvas: PongCanvas)
+	constructor(canvas: PongCanvas, score: Score)
 	{
-		super({x:0, y: 20}, 20, new Vector2D(0,0));
+		super({x:50, y: 50}, 20, new Vector2D(3,1));
 		this.texture = new Image();
 		this.texture.src = "/pong_ball.png";
 		this.canvas = canvas;
+		this.score = score;
 	}
 
 	getTexture() : HTMLImageElement
@@ -214,7 +235,6 @@ export class PongBall extends PhysicObject
 			this.position.y - line_position.y,
 			normal.getX(), normal.getY()
 		);
-		console.log("distance from line", distance_from_line);
 		if (distance_from_line < (this.size as number) / 2)
 		{
 			let speed_normal:number = dot_product(this.speed.getX(), this.speed.getY(), normal.getX(), normal.getY());
@@ -224,11 +244,61 @@ export class PongBall extends PhysicObject
 		}
 	}
 
+	test_collide_score(line_position: Position,normal: Vector2D)
+	{
+		var distance_from_line = dot_product(
+			this.position.x - line_position.x,
+			this.position.y - line_position.y,
+			normal.getX(), normal.getY()
+		);
+
+		var next_side;
+		if (distance_from_line < (this.size as number) / 2)
+		{
+			next_side = 1;
+			if (this.pos.x <= (this.size as number))
+				this.score.p2++;
+			else if (this.pos.x >= this.canvas.canvas.width - (this.size as number))
+			{
+				this.score.p1++;
+				next_side = -1;
+			}
+			this.pos.x = this.canvas.canvas.width / 2;
+			this.pos.y = this.canvas.canvas.height / 2;
+			let new_dir:number = Math.random() * 5 * ((Math.random() * 2) - 1);
+			console.log("angle",new_dir);
+			console.log("Score",this.score);
+			this.speed.setX = (3 * next_side);
+			this.speed.setY = (new_dir);
+			console.log("speed", this.speed);
+		}
+	}
+
+	test_collide_paddle(paddle_position: Position,normal: Vector2D)
+	{
+		var distance_from_line = dot_product(
+			this.position.x - paddle_position.x,
+			this.position.y - paddle_position.y,
+			normal.getX(), normal.getY()
+		);
+		if ((distance_from_line < (this.size as number) / 2) && ((paddle_position.y - this.position.y) > -25 && (paddle_position.y - this.position.y) < 25))
+		{
+			let speed_normal:number = dot_product(this.speed.getX(), this.speed.getY(), normal.getX(), normal.getY());
+			let speed_tangent:number = dot_product(this.speed.getX(), this.speed.getY(), normal.getY(), -normal.getX());
+			this.speed.setX = -(speed_normal * normal.getX()) + (speed_tangent * normal.getY());
+			this.speed.setY = -(speed_normal * normal.getY()) + (speed_tangent * -normal.getX());
+			this.speed.coef_product(1.05);
+		}
+	}
+
 	updatePos(): void
 	{
-
 		this.test_collide({x: 0, y: 0}, this.canvas.top_normal);
 		this.test_collide({x: 0, y: this.canvas.canvas.height}, this.canvas.bottom_normal);
+		this.test_collide_paddle({x: this.canvas.canvas.width - 20, y: this.canvas.canvas.height / 2}, this.canvas.right_normal);
+		this.test_collide_paddle({x: 20, y: this.canvas.canvas.height / 2}, this.canvas.left_normal);
+		this.test_collide_score({x: 0, y: 0}, this.canvas.left_normal);
+		this.test_collide_score({x: this.canvas.canvas.width, y: 0}, this.canvas.right_normal);
 		this.position.x += this.speed.getX();
 		this.position.y += this.speed.getY();
 	}
