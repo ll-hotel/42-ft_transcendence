aab9e0204025a537622f5493352cc2ebce233346
Nassim Yacoubi <nyacoubi@z1r8p4.42lyon.fr>
2026-01-20 12:01:09 +0100

    Feat: add another notifs style bottom right of page

--- frontend/src/pages/utils/notifs.ts
+++ frontend/src/pages/utils/notifs.ts
@@ -3,7 +3,9 @@ export function notify(message: string, type: "success" | "error" | "info") {
   if (!container) {
     container = document.createElement("div");
     container.id = "notif-container";
-    container.className = "fixed top-4 right-4 z-50 space-y-2";
+    container.className = "fixed bottom-4 right-4 z-50 flex flex-col-reverse space-y-reverse space-y-2";
+// container.className = "fixed top-4 right-4 z-50 space-y-2" top right notifs
+
     document.body.appendChild(container);
   }
 
@@ -12,10 +14,28 @@ export function notify(message: string, type: "success" | "error" | "info") {
     container.children[0].remove();
 
   const el = document.createElement("div");
-  el.className = `px-4 py-2 rounded text-white font-semibold shadow ${
-    type === "success" ? "bg-green-600" : type === "error" ? "bg-red-600" : "bg-orange-600" }`;
+
+  let color = "";
+  switch (type) {
+    case "success":
+      color = "bg-green-600";
+      break;
+    case "error":
+      color = "bg-red-600";
+      break;
+    case "info":
+      color = "bg-orange-600";
+      break;
+  }
+  
+  el.className = `px-4 py-2 rounded text-white font-semibold shadow ${color}`;
   el.textContent = message;
   container.appendChild(el);
   setTimeout(() => el.remove(), 3000);
 }
+
+
+
+//container.className = "fixed bottom-4 right-4 z-50 flex flex-col-reverse space-y-reverse space-y-2";
+
 
